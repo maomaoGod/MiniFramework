d0de7d66a73818d07f753667ff3de9a344181d27
公司 <1413698201@qq.com>
2018-07-31 16:49:27 +0800

    null

---
+++ Assets/MiniFramework/04.ResKit/HttpDownloader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MiniFramework
+{
+    class HttpDownloader : MonoBehaviour
+    {
+        void Start()
+        {
+           
+        }
+        public bool Get(string url,string savePath)
+        {
+            StartCoroutine(WebDownload());
+            return true;
+        }
+        IEnumerator WebDownload()
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get("file:///"+Application.streamingAssetsPath+ "/Xml"))
+            {
+                yield return www.Send();
+
+                if (www.isError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    // Show results as text
+                    Debug.Log(www.downloadHandler.text);
+                    // Or retrieve results as binary data
+                    byte[] results = www.downloadHandler.data;
+                }
+            }
+        }
+    }
+}
